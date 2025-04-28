595d1e7e2ea3846791db4bc769f0ff7b23568f68
Ahmed Shafique <99950606+AhmedShafique313@users.noreply.github.com>
2025-04-28 18:27:20 +0500

    Making a demo software for the project

--- firecrawl_Api.py
+++ firecrawl_Api.py
@@ -4,37 +4,37 @@ from env_load import firecrawl_api_key
 
 def scrapping_function(website_url):
     app = FirecrawlApp(api_key=firecrawl_api_key)
-    # specific_pages = [
-    #     website_url,  # Main page
-    #     f"{website_url}/about-us",
-    #     f"{website_url}/services",
-    #     f"{website_url}/careers",
-    #     f"{website_url}/portfolio",
-    #     f"{website_url}/contact"
-    # ]
     specific_pages = [
-        website_url,  # Home
+        website_url,  # Main page
         f"{website_url}/about-us",
-        f"{website_url}/contact",
         f"{website_url}/services",
-        f"{website_url}/products",
-        # f"{website_url}/blog",
-        # f"{website_url}/careers",
-        # f"{website_url}/portfolio",
-        # f"{website_url}/pricing",
-        # f"{website_url}/team",
-        # f"{website_url}/privacy-policy",
-        # f"{website_url}/landing-page",
-        # f"{website_url}/events",
-        # f"{website_url}/partners",
-        # f"{website_url}/support",
-        # f"{website_url}/login",
-        # f"{website_url}/community",
-        # f"{website_url}/sitemap",
-        # f"{website_url}/accessibility",
-        # f"{website_url}/announcements",
-        # f"{website_url}/offers"
+        f"{website_url}/careers",
+        f"{website_url}/portfolio",
+        f"{website_url}/contact"
     ]
+    # specific_pages = [
+    #     website_url,  # Home
+    #     f"{website_url}/about-us",
+    #     f"{website_url}/contact",
+    #     f"{website_url}/services",
+    #     # f"{website_url}/products",
+    #     # f"{website_url}/blog",
+    #     # f"{website_url}/careers",
+    #     # f"{website_url}/portfolio",
+    #     # f"{website_url}/pricing",
+    #     # f"{website_url}/team",
+    #     # f"{website_url}/privacy-policy",
+    #     # f"{website_url}/landing-page",
+    #     # f"{website_url}/events",
+    #     # f"{website_url}/partners",
+    #     # f"{website_url}/support",
+    #     # f"{website_url}/login",
+    #     # f"{website_url}/community",
+    #     # f"{website_url}/sitemap",
+    #     # f"{website_url}/accessibility",
+    #     # f"{website_url}/announcements",
+    #     # f"{website_url}/offers"
+    # ]
     scraped_data = {}
     # for page in specific_pages:
     #     scrape_result = app.scrape_url(page, params={'formats': ['markdown']})
