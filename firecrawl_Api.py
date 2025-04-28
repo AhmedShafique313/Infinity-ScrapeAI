edb0ec8ff4992e8ac0e029ab1ee69829e88eabd9
Ahmed Shafique <99950606+AhmedShafique313@users.noreply.github.com>
2025-04-28 18:44:49 +0500

    Now its time to complete this entire project today

--- firecrawl_Api.py
+++ firecrawl_Api.py
@@ -12,40 +12,8 @@ def scrapping_function(website_url):
         f"{website_url}/portfolio",
         f"{website_url}/contact"
     ]
-    # specific_pages = [
-    #     website_url,  # Home
-    #     f"{website_url}/about-us",
-    #     f"{website_url}/contact",
-    #     f"{website_url}/services",
-    #     # f"{website_url}/products",
-    #     # f"{website_url}/blog",
-    #     # f"{website_url}/careers",
-    #     # f"{website_url}/portfolio",
-    #     # f"{website_url}/pricing",
-    #     # f"{website_url}/team",
-    #     # f"{website_url}/privacy-policy",
-    #     # f"{website_url}/landing-page",
-    #     # f"{website_url}/events",
-    #     # f"{website_url}/partners",
-    #     # f"{website_url}/support",
-    #     # f"{website_url}/login",
-    #     # f"{website_url}/community",
-    #     # f"{website_url}/sitemap",
-    #     # f"{website_url}/accessibility",
-    #     # f"{website_url}/announcements",
-    #     # f"{website_url}/offers"
-    # ]
     scraped_data = {}
-    # for page in specific_pages:
-    #     scrape_result = app.scrape_url(page, params={'formats': ['markdown']})
-    #     scraped_data[page] = scrape_result.get('markdown', 'No content available')
-    # with open('scraped_data.json', 'w', encoding='utf-8') as md_file:
-    #     json.dump(scraped_data, md_file, ensure_ascii=False, indent=4)
-    # return scraped_data
     for page in specific_pages:
-            try:
-                scrape_result = app.scrape_url(page, params={'formats': ['markdown']})
-            except Exception as e:
-                print(f"❌ {page}: {str(e)}")
-
-scrapping_function('designgaga.ca')
+        scrape_result = app.scrape_url(page, params={'formats': ['markdown']})
+        scraped_data[page] = scrape_result.get('markdown', 'No content available')
+    return scraped_data
